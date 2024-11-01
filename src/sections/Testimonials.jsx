02f33cfd5e59f81e4b9233fc134a69fa320d069f
MauroTre <m.gutierrez7@hotmail.com>
2024-10-31 22:11:13 -0500

    testimonials section

---
+++ src/sections/Testimonials.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Element } from "react-scroll";
+
+const Testimonials = () => {
+  return (
+    <section>
+      <Element name="testimonials">
+        <div className="container"></div>
+      </Element>
+    </section>
+  );
+};
+
+export default Testimonials;
