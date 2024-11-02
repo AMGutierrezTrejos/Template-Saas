0c742740cc7494e53a27db570dd0ab3c06e69f11
MauroTre <m.gutierrez7@hotmail.com>
2024-11-01 23:05:21 -0500

    testimonials

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -6,6 +6,7 @@ const Testimonials = () => {
     <section>
       <Element name="testimonials">
         <div className="container"></div>
+        <h1>Testimonials</h1>
       </Element>
     </section>
   );
